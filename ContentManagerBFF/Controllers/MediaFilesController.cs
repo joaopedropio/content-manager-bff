a6a975460def9118052dbf5aa09dd53ba5269b7a
Joao Pedro Pio <joao-pio@hotmail.com>
2019-04-07 23:30:38 -0300

    add mediaifles path

---
+++ ContentManagerBFF/Controllers/MediaFilesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContentManagerBFF.Controllers
+{
+    public class MediaFilesController : Controller
+    {
+        private ContentClient.ContentClient client;
+
+        public MediaFilesController()
+        {
+            var contentApiUrl = new Configuration().ContentAPIURL;
+            this.client = new ContentClient.ContentClient(contentApiUrl);
+        }
+
+        [Route("/mediafiles")]
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return await this.client.GetMediaFilesPaths();
+        }
+    }
+}
